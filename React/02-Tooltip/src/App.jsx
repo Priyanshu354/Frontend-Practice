02a4c621db2448ff9b1960bc372da9acd4241446
Priyanshu354 <titpriyanshuraj@gmail.com>
2025-08-23 12:12:26 +0530

    new format

--- React/02-Tooltip/src/App.jsx
+++ React/02-Tooltip/src/App.jsx
@@ -1,12 +1,10 @@
 import { useState } from 'react'
 import Tooltip from './Tooltip'
-import TooltipRev from './Revison/TooltipRev'
 
 function App() {
   return (
     <>
-    {/* <Tooltip/> */}
-    <TooltipRev/>
+    <Tooltip/>
     </>
   )
 }
